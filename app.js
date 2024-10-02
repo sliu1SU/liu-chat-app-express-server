456fcdb84f2fceaa859ecd95e66a2b91ea1e0c44
Sizhe Liu <sliu1@seattleu.edu>
2024-10-02 16:23:51 -0700

    added "onAuthStateChanged"and apis to handle to sign in and log in.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+// Middleware to parse JSON
+app.use(bodyParser.json());
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth, onAuthStateChanged} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+const { initializeApp } = require("firebase/app");
+const {getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged} = require('firebase/auth');
+const {getFirestore, doc, getDoc} = require('firebase/firestore');
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,9 +30,25 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(firebaseApp);
-const auth = getAuth(firebaseApp);
+const firebaseAuth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
+
+// fn to monitor current signed in user
+onAuthStateChanged(firebaseAuth, function(user) {
+    try {
+        if (user) {
+            // User is signed in
+            const uid = user.uid;
+            // Additional logic for when the user is signed in
+            console.log("User ID:", uid);
+        } else {
+            // User is signed out
+            console.log("No user is signed in.");
+        }
+    } catch (error) {
+        console.error("Error handling authentication state:", error);
+    }
+});
 
 app.listen(PORT, (error) =>{
     if(!error)
@@ -39,19 +60,47 @@ app.listen(PORT, (error) =>{
 
 app.get('/', async (req, res)=>{
     // access firebase data here
-    const welcomeMsgRef = db.collection('weekone').doc('testmessage');
-    const doc = await welcomeMsgRef.get();
-    if (!doc.exists) {
+    const welcomeMsgRef = doc(db, 'weekone', 'testmessage');
+    const document = await getDoc(welcomeMsgRef);
+    if (!document.exists) {
         res.status(400);
         res.send("message is not found in Firebase");
     } else {
-        const data = doc.data();
+        // if doc is found, send the message to front end
+        const data = document.data();
         res.status(200);
         res.send(data.message);
     }
 });
 
 // need 3 apis for authentication
+// api handles sign up
+app.post('/signup/', async (req, res)=>{
+    const {email, password} = req.body;
+    try {
+        // note: user will be log in if sign up succeeds
+        //console.log("trigger sign up api");
+        const userCredential  = await createUserWithEmailAndPassword(firebaseAuth, email, password);
+        const user = userCredential.user;
+        res.status(200);
+        res.send(user);
+    } catch (e) {
+        res.status(400);
+        res.send(e);
+    }
+});
 
-
+// api handles log in
+app.post('/login/', async (req, res)=>{
+    const {email, password} = req.body;
+    try {
+        const userCredential  = await signInWithEmailAndPassword(firebaseAuth, email, password);
+        const user = userCredential.user;
+        res.status(200);
+        res.send(user);
+    } catch (e) {
+        res.status(400);
+        res.send(e);
+    }
+});
 
